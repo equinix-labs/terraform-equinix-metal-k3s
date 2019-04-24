6c755a5b7f476e05fa6053115ef436c8185f0a56
Joseph D. Marhee <joseph@packet.com>
2019-04-24 11:04:13 -0500

    Adds RBAC, user creation demo--will likely need reworking, use the best-practice cfssl, etc. tools

--- example/create_inventory.sh
+++ example/create_inventory.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[controllers]" | tee deploy_demo/inventory.yaml && \
+echo "[controllers]" | tee demos/inventory.yaml && \
 
 for host in `cd ../ ; terraform state list | grep primary | xargs -n1 -I% terraform state show % | grep network.0.address | awk '{print $3}'` ; do \
-	echo root@$host | tee -a deploy_demo/inventory.yaml ; done
+	echo root@$host | tee -a demos/inventory.yaml ; done
