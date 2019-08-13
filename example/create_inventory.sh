a1d235527933dfacb35a7f3ca2adf243639674b3
Joseph Marhee <jmarhee@interiorae.com>
2019-08-13 15:44:12 -0500

    updates inventory generate script since the for_each behavior now requires a new resource identifier, this can be cleane dup later, but functions now consistently

--- example/create_inventory.sh
+++ example/create_inventory.sh
@@ -2,5 +2,6 @@
 
 echo "[controllers]" | tee demos/inventory.yaml && \
 
-for host in `cd ../ ; terraform state list | grep primary | xargs -n1 -I% terraform state show % | grep network.0.address | awk '{print $3}'` ; do \
+#for host in `cd ../ ; terraform state list | grep primary | xargs -n1 -I% terraform state show % | grep network.0.address | awk '{print $3}'` ; do \
+for host in `cd ../ ; grep access_public_ipv4 terraform.tfstate | awk '{print $2}' | sed -e 's|"||g' -e 's|,||g'`; do
 	echo root@$host | tee -a demos/inventory.yaml ; done
